6f0152c8e9269e15665e9644a387e69e46b8241f
Shashank Mittal <mittals@codeaurora.org>
2012-06-19 19:45:35 -0700

    mm: Fix a compiler warning.
    Fix compiler warning for a variable not initialized.
    
    Change-Id: Ieedeb1cfb5a22eb5f671e6bfd1361315347a49af
    Signed-off-by: Shashank Mittal <mittals@codeaurora.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -541,7 +541,7 @@ static inline void __free_one_page(struct page *page,
 	unsigned long page_idx;
 	unsigned long combined_idx;
 	unsigned long uninitialized_var(buddy_idx);
-	struct page *buddy;
+	struct page *buddy = NULL;
 	unsigned int max_order;
 
 	max_order = min_t(unsigned int, MAX_ORDER, pageblock_order + 1);
