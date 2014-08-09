44a2b8abd35b7decc3bd8b04be5457a45ba4e811
Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
2014-08-09 11:15:30 +0530

    printk: Add function to return log buffer address and size
    Platforms like IBM Power Systems supports service processor
    assisted dump. It provides interface to add memory region to
    be captured when system is crashed.
    
    During initialization/running we can add kernel memory region
    to be collected.
    
    Presently we don't have a way to get the log buffer base address
    and size. This patch adds support to return log buffer address
    and size.
    
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Acked-by: Andrew Morton <akpm@linux-foundation.org>
    Change-Id: Ie22359d01ca51bb735285f7dc10b18c899931485
    Signed-off-by: Julian Veit <claymore1298@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -307,6 +307,17 @@ static u32 __log_align __used = LOG_ALIGN;
 
 /* cpu currently holding logbuf_lock */
 static volatile unsigned int logbuf_cpu = UINT_MAX;
+/* Return log buffer address */
+char *log_buf_addr_get(void)
+{
+	return log_buf;
+}
+
+/* Return log buffer size */
+u32 log_buf_len_get(void)
+{
+	return log_buf_len;
+}
 
 /* human readable text of the record */
 static char *log_text(const struct printk_log *msg)
