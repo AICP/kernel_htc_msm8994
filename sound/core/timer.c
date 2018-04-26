ad65cc3fc48941a5c782f945fbff0a24fd41ccdf
Corinna Vinschen <xda@vinschen.de>
2018-04-26 22:41:39 +0200

    ALSA: timer: fix a missing goto
    "ALSA: timer: Handle disconnection more safely" was applied
    after "ALSA: timer: Fix race at concurrent reads", which
    left a break in place of the correct goto _error.
    
    Fix that.
    
    Thanks to Elektroschmock <elektroschmock78@googlemail.com> for
    pointing out the problem.
    
    Change-Id: I85252c1d40c4bd88ca86fa8c0bb228d7ed6b12ed
    Signed-off-by: Corinna Vinschen <xda@vinschen.de>

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1980,7 +1980,7 @@ static ssize_t snd_timer_user_read(struct file *file, char __user *buffer,
 
 			if (tu->disconnected) {
 				err = -ENODEV;
-				break;
+				goto _error;
 			}
 			if (signal_pending(current)) {
 				err = -ERESTARTSYS;
