fbad6d17b0f6d1f1805ab573c4912b3fc77372d8
Julian Veit <claymore1298@gmail.com>
2018-07-07 21:09:48 +0200

    Fix compilation errors on 3.10.105

--- lib/genalloc.c
+++ lib/genalloc.c
@@ -289,7 +289,7 @@ u64 gen_pool_alloc_aligned(struct gen_pool *pool, size_t size,
 	struct gen_pool_chunk *chunk;
 	u64 addr = 0, align_mask = 0;
 	int order = pool->min_alloc_order;
-	int nbits, start_bit, end_bit, remain;
+	int nbits, start_bit, remain;
 
 #ifndef CONFIG_ARCH_HAVE_NMI_SAFE_CMPXCHG
 	BUG_ON(in_nmi());
