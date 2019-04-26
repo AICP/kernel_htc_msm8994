f948d33fc391a1160ceca009806bbad37553ead7
Julian Veit <claymore1298@gmail.com>
2019-04-26 23:28:29 +0200

    i2c: reduce Sensorhub spamming
    make those messages to debug not info:
    [   68.554604] [S_HUB] irq_hndl: INT_st(1, 2, 3, 4) = (0x0, 0x0, 0x0, 0x0), err_st = 0x0, bch_st = 0x4
    [   68.554673] [S_HUB] w_batch_read ++
    [   68.557495] [S_HUB] w_batch_read --
    [   68.654300] [S_HUB] irq_hndl: INT_st(1, 2, 3, 4) = (0x0, 0x0, 0x0, 0x0), err_st = 0x0, bch_st = 0x4
    [   68.654362] [S_HUB] w_batch_read ++
    [   68.660289] [S_HUB] w_batch_read --
    
    Change-Id: Ic7dc4ffbb18b2b9c7006a7b8f2d4a79ad108b449

--- drivers/i2c/chips/CwMcuSensor.c
+++ drivers/i2c/chips/CwMcuSensor.c
@@ -4329,7 +4329,7 @@ static irqreturn_t cwmcu_irq_handler(int irq, void *handle)
 	ret = CWMCU_i2c_read(mcu_data, CWSTM32_BATCH_MODE_COMMAND, &batch_st, 1);
 	if (ret < 0)batch_st = 0;
 
-	I(
+	D(
 	  "irq_hndl: INT_st(1, 2, 3, 4) = (0x%x, 0x%x, 0x%x, 0x%x), err_st = 0x%x"
 	  ", bch_st = 0x%x\n",
 	  INT_st1, INT_st2, INT_st3, INT_st4, err_st, batch_st);
@@ -5754,7 +5754,7 @@ static void cwmcu_one_shot(struct work_struct *work)
 #endif
 
 	if (mcu_data->w_batch_read == true) {
-		I("w_batch_read ++ \n");
+		D("w_batch_read ++ \n");
 		mcu_data->w_batch_read = false;
 
 		wake_lock(&mcu_data->batch_read_wake_lock);
@@ -5765,7 +5765,7 @@ static void cwmcu_one_shot(struct work_struct *work)
 		cwmcu_powermode_switch(mcu_data, 0);
 
 		wake_unlock(&mcu_data->batch_read_wake_lock);
-		I("w_batch_read -- \n");
+		D("w_batch_read -- \n");
 	}
 
 	wake_unlock(&mcu_data->one_shot_wake_lock);
