2bfe30d75d4ada63964594daf95a22d0410e35f5
Julian Veit <claymore1298@gmail.com>
2019-06-04 10:12:47 +0200

    Revert "Staging: android: binder: Ratelimit binder debug messages"
    This reverts commit 1b3dd151297b1322a16295767142fe12bed19687.
    
    Revert to apply the upstream solution
    
    Change-Id: Ia8479f7ab360da8e4168c14604c96472f0e7ba89

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -39,7 +39,6 @@
 #include <linux/slab.h>
 #include <linux/pid_namespace.h>
 #include <linux/security.h>
-#include <linux/ratelimit.h>
 
 #ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
 #define BINDER_IPC_32BIT 1
@@ -132,7 +131,7 @@ module_param_call(stop_on_user_error, binder_set_stop_on_user_error,
 #define binder_debug(mask, x...) \
 	do { \
 		if (binder_debug_mask & mask) \
-			pr_info_ratelimited(x); \
+			pr_info(x); \
 	} while (0)
 
 #define binder_user_error(x...) \
